e47092168b2a60685bfb7061eca653ad669ad3e8
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-05-31 14:43:06 -0600

    formats python with black

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,12 @@
 Calulator Functions
 """
 
-def add(x , y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
 
-def subtract(x , y):
-    """ Subtract x from y return result."""
+def subtract(x, y):
+    """Subtract x from y return result."""
     return y - x
