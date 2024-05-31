4dca54c447fc3b65baa58c5ba6ac2ced88b6f906
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-05-31 14:16:58 -0600

    first tests

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calulator Functions
+"""
+
+def add(x , y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x , y):
+    """ Subtract x from y return result."""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the Calc mdule."""
+
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
