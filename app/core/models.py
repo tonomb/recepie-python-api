b800de4f4f02c222d7fc265ac0c062c3d3c7ed10
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-06-04 17:22:14 -0600

    created custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -15,8 +15,20 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_field):
+        if not email:
+            raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and reutrn a new superuser"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
